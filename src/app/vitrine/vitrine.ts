54a333ffded7f4b4071e1153f8e4bf30b33695b0
ketlinvgp1 <ketlinvgp@gmail.com>
2026-09-17 14:56:33 -0300

    Vitrine

--- src/app/vitrine/vitrine.ts
+++ src/app/vitrine/vitrine.ts
@@ -1,10 +1,105 @@
 import { Component } from '@angular/core';
-import { RouterLink, } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { Produto } from '../model/produto';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [ RouterLink],
+  imports: [ RouterLink, CommonModule],
   selector: 'app-vitrine',
   styleUrl: './vitrine.css',
   templateUrl: './vitrine.html',
 })
-export class Vitrine {}
+export class Vitrine {
+    lista: Produto[] = [
+   {
+        codigo: 1,
+        nome: "São Luís – MA",
+        descritivo: "Travessia dos Lençóis Maranhenses",
+        quantidade: 10,
+        valor: 1899.90,
+        promo: 1699.90,
+        destaque: 1
+    },
+    {
+        codigo: 2,
+        nome: "Manaus – AM",
+        descritivo: "Encontro das Águas",
+        quantidade: 10,
+        valor: 1599.90,
+        promo: 1399.90,
+        destaque: 1
+    },
+    {
+        codigo: 3,
+        nome: "Recife – PE",
+        descritivo: "Praia de Boa Viagem",
+        quantidade: 10,
+        valor: 1299.90,
+        promo: 1199.90,
+        destaque: 0
+    },
+    {
+        codigo: 4,
+        nome: "Rio de Janeiro – RJ",
+        descritivo: "Cristo Redentor e praias do Rio de Janeiro",
+        quantidade: 10,
+        valor: 1499.90,
+        promo: 1299.90,
+        destaque: 1
+    },
+    {
+        codigo: 5,
+        nome: "Foz do Iguaçu – PR",
+        descritivo: "Visita às Cataratas do Iguaçu",
+        quantidade: 10,
+        valor: 1399.90,
+        promo: 1199.90,
+        destaque: 1
+    },
+    {
+        codigo: 6,
+        nome: "Jalapão – TO",
+        descritivo: "Fervedouros, dunas e paisagens do Jalapão",
+        quantidade: 10,
+        valor: 1799.90,
+        promo: 1599.90,
+        destaque: 0
+    },
+    {
+        codigo: 7,
+        nome: "Bonito – MS",
+        descritivo: "Flutuação em rios de águas cristalinas",
+        quantidade: 10,
+        valor: 1699.90,
+        promo: 1499.90,
+        destaque: 1
+    },
+    {
+        codigo: 8,
+        nome: "Ouro Preto – MG",
+        descritivo: "Centro histórico e arquitetura colonial",
+        quantidade: 10,
+        valor: 999.90,
+        promo: 899.90,
+        destaque: 0
+    },
+    {
+        codigo: 9,
+        nome: "Natal – RN",
+        descritivo: "Praias, dunas e passeio de buggy",
+        quantidade: 10,
+        valor: 1399.90,
+        promo: 1199.90,
+        destaque: 1
+    },
+    {
+        codigo: 10,
+        nome: "Ilhabela – SP",
+        descritivo: "Mergulho e praias paradisíacas",
+        quantidade: 10,
+        valor: 1199.90,
+        promo: 999.90,
+        destaque: 0
+    }
+];
+}
